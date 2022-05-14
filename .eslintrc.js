324b982c04769f84c7ce04d45b40f52e1dba2f60
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2022-05-14 11:19:29 -0300

    feat: add new rules to eslint and babel configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,29 @@
 module.exports = {
-  root: true,
-  extends: ['@react-native-community', 'airbnb-typescript', 'prettier', 'prettier/@typescript-eslint', 'prettier/react'],
+    root: true,
+    extends: [
+        'plugin:import/recommended',
+        '@react-native-community',
+        'airbnb-typescript',
+        'prettier'
+    ],
+    parserOptions: {
+        project: ['./tsconfig.json']
+    },
+    ignorePatterns: ['.eslintrc.js'],
+    rules: {
+        // suppress errors for missing 'import React' in files
+        'react/react-in-jsx-scope': 'off',
+        // allow jsx syntax in js files (for next.js project)
+        'react/jsx-filename-extension': [
+            1,
+            { extensions: ['.js', '.jsx', '.ts', '.tsx'] }
+        ], //should add ".ts" if typescript project
+        'import/no-unresolved': [0, { commonjs: true, amd: true }],
+        'import/named': 0,
+        'import/no-named-as-default': 0,
+        'import/no-named-as-default-member': 0,
+        'import/namespace': 0,
+        'import/default': 0,
+        'import/export': 0
+    }
 };
