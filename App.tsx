91cfbdd930fdacf0d4262e96b8f4a9b7176176a9
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2022-05-15 17:19:41 -0300

    feat: add fonts and AppLoading component

--- App.tsx
+++ App.tsx
@@ -1,16 +1,23 @@
-import { View, StyleSheet } from 'react-native';
+import React from 'react';
+import { useFonts } from 'expo-font';
+import { Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
+import {
+    Rajdhani_500Medium,
+    Rajdhani_700Bold
+} from '@expo-google-fonts/rajdhani';
+import AppLoading from 'expo-app-loading';
 import { SignIn } from './src/screens/SignIn';
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1
-    }
-});
-
 export default function App(): JSX.Element {
-    return (
-        <View style={styles.container}>
-            <SignIn />
-        </View>
-    );
+    const [fontsLoaded] = useFonts({
+        Inter_400Regular,
+        Inter_500Medium,
+        Rajdhani_500Medium,
+        Rajdhani_700Bold
+    });
+
+    if (!fontsLoaded) {
+        return <AppLoading />;
+    }
+    return <SignIn />;
 }
