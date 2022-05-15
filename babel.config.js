1377f54da5d7fd3f49766f1f55951856e4f7aadc
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2022-05-15 16:31:24 -0300

    feat: update babel and tsconfig

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,6 @@
 module.exports = function (api) {
     api.cache(true);
     return {
-        presets: ['babel-preset-expo'],
-        resolve: {
-            extensions: ['.js', '.jsx', '.tsx']
-        }
+        presets: ['babel-preset-expo']
     };
 };
