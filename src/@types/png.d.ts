849245537fe898e99b4b0fadc9bf72c8538b5530
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2022-05-15 16:36:10 -0300

    fix: remove unused react import

--- src/@types/png.d.ts
+++ src/@types/png.d.ts
@@ -1,5 +1,4 @@
 declare module '*.png' {
-    import React from 'react';
     import { ImageSourcePropType } from 'react-native-svg';
     const content: ImageSourcePropType;
     export default content;
