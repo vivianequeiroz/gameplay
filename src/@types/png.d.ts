8a613d08b633e8814390f6ffb5172560e5889322
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2022-05-15 16:27:19 -0300

    feat: add types for svg and png images

---
+++ src/@types/png.d.ts
@@ -0,0 +1,6 @@
+declare module '*.png' {
+    import React from 'react';
+    import { ImageSourcePropType } from 'react-native-svg';
+    const content: ImageSourcePropType;
+    export default content;
+}
