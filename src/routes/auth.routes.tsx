e1b1f1ce7dbc0cf62580d0c37cdf18c3488bedc8
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2022-05-17 20:08:59 -0300

    wip: add routes to home and signin screens

---
+++ src/routes/auth.routes.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import { SignIn } from '../screens/SignIn';
+import { Home } from '../screens/Home';
+
+const { Navigator, Screen } = createStackNavigator();
+
+export function AuthRoutes() {
+    return (
+        <Navigator
+            screenOptions={{
+                cardStyle: { backgroundColor: 'transparent' },
+                headerShown: false
+            }}
+        >
+            <Screen name="SignIn" component={SignIn} />
+            <Screen name="Home" component={Home} />
+        </Navigator>
+    );
+}
