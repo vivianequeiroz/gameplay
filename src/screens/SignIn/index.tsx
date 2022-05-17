f7ed20f5e32b7b0f8ed0bd7cacc748e0f09a5aaa
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2022-05-17 20:10:21 -0300

    feat(wip): add method to handle user signin and redirect to home page

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -3,8 +3,16 @@ import { Text, View, Image } from 'react-native';
 import { styles } from './styles';
 import { ButtonIcon } from '../../components/ButtonIcon';
 import IllustrationImg from '../../assets/illustration.png';
+import { useNavigation } from '@react-navigation/native';
+import { StackNavigationProp } from '@react-navigation/stack';
+//1h13
 
 export function SignIn(): JSX.Element {
+    const navigation = useNavigation<StackNavigationProp<any>>();
+    function handleSignIn() {
+        navigation.navigate('Home');
+    }
+
     return (
         <View style={styles.container}>
             <Image
@@ -25,7 +33,10 @@ export function SignIn(): JSX.Element {
                     your favorites games with friends
                 </Text>
 
-                <ButtonIcon title={'Log in with discord'} activeOpacity={0.7} />
+                <ButtonIcon
+                    title={'Log in with discord'}
+                    onPress={handleSignIn}
+                />
             </View>
         </View>
     );
